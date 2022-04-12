36909a114ce0ff859efcd4263ec9eda2bbfe4506
sheyna <sheynawatkins@gmail.com>
2022-04-12 12:01:08 -0700

    Added people date and greeting functionality and styles.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Person from './Person.js';
 import Header from './Header.js';
+import Main from './Main';
 import './App.css';
 
 class App extends React.Component {
@@ -9,16 +10,7 @@ class App extends React.Component {
     return (
       <>
         <Header/>
-        <main>
-          <Person 
-             name="Sheyna" 
-             haircolor="brown" 
-             hometown="Seattle"
-          />
-          <Person name="Chad"/>
-          <Person name="Michael"/>
-          <Person name="Roger"/>
-        </main>
+        <Main/>
       </>
     )
   }
